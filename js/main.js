59507069eea4a38fec2b72f8fceee5fc01c1ed12
Comrade <57000785+asmfahim@users.noreply.github.com>
2021-08-20 07:19:12 +0600

    First Portfolio

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Gallery
+
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+          openEffect: "none",
+          closeEffect: "none"
+      });
+      
+      $(".zoom").hover(function(){
+          
+          $(this).addClass('transition');
+      }, function(){
+          
+          $(this).removeClass('transition');
+      });
+  });
+      
